ee817a0f70fd9726a184d267a1bef3d56ac2ac8f
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-11-25 18:29:06 -0500

    Agregar personaje

--- main.py
+++ main.py
@@ -11,12 +11,24 @@ pygame.display.set_caption("Invasion de amor")
 icono = pygame.image.load("avion-de-papel.png")
 pygame.display.set_icon(icono)
 
+# Jugador
+tilincha = pygame.image.load("mujer.png")
+jugador_x = 368
+jugador_y = 536
+
+def jugador():
+    pantalla.blit(tilincha, (jugador_x, jugador_y))
+
 # Loop del Juego
 se_ejecuta = True
 
 while se_ejecuta:
+    # RGB
+    pantalla.fill((205,144,255))
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-    pantalla.fill((205,144,255))
+
+    jugador()
+
     pygame.display.update()
