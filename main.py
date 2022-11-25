fa0b0a000dcebdb04177b25abad33d8e75af2b56
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-11-25 18:16:17 -0500

    Cambiar titulo, icono y color

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 
+# Inicializa Pygame
 pygame.init()
 
+# Pantalla y dimenciones
 pantalla = pygame.display.set_mode((800, 600))
 
+# Titulo e Icono
+pygame.display.set_caption("Invasion de amor")
+icono = pygame.image.load("avion-de-papel.png")
+pygame.display.set_icon(icono)
+
+# Loop del Juego
 se_ejecuta = True
+
 while se_ejecuta:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
+    pantalla.fill((205,144,255))
+    pygame.display.update()
