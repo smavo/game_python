dfe43afd85ad6c4418ae35b790638c7fdb3dc469
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-11-25 20:08:47 -0500

    Agregando movimiento al disparar

--- main.py
+++ main.py
@@ -75,7 +75,9 @@ while se_ejecuta:
                 # print("Tecla Derecha presionada")
                 jugador_x_cambio = 0.1
             elif evento.key == pygame.K_SPACE:
-                disparar_bala(jugador_x, bala_y)
+                if not bala_visible:
+                    bala_x = jugador_x
+                    disparar_bala(bala_x, bala_y)
 
         # Evento detectar soltar flecha
         if evento.type == pygame.KEYUP:
@@ -104,8 +106,12 @@ while se_ejecuta:
         enemigo_y += enemigo_y_cambio
 
     # Movimiendo Bala
+    if bala_y <= -64:
+        bala_y = 530
+        bala_visible = False
+
     if bala_visible:
-        disparar_bala(jugador_x, bala_y)
+        disparar_bala(bala_x, bala_y)
         bala_y -= bala_y_cambio
 
     jugador(jugador_x, jugador_y)
